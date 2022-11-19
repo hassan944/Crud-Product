073d539685b85bad324d25164b66303c2b9b13bd
Furkan <furkhann25@gmail.com>
2022-11-19 11:29:56 +0530

    crud project

---
+++ ProductInsert.js
@@ -0,0 +1,37 @@
+import { useFormik } from "formik";
+import axios from "axios";
+
+export function ProductInsert(){
+
+    const formik = useFormik({
+        initialValues: {
+            ProductId:0,
+            Name:"",
+            Price:0,
+            Rating:0
+        },
+        onSubmit: (values)=>{
+            axios.post("http://localhost:4000/addproduct",values);
+            alert("Record Inserted");
+        }
+    });
+
+    return(
+        <div>
+           <form onSubmit={formik.handleSubmit}>
+           <h3>Add New Product</h3>
+            <dl>
+                <dt>Product Id</dt>
+                <dd><input type="text" value={formik.values.ProductId} onChange={formik.handleChange} name="ProductId"/></dd>
+                <dt>Name</dt>
+                <dd><input name="Name" onChange={formik.handleChange} value={formik.values.Name} type="text"/></dd>
+                <dt>Price</dt>
+                <dd><input type="text" name="Price" value={formik.values.Price} onChange={formik.handleChange}/></dd>
+                <dt>Rating</dt>
+                <dd><input type="number" name="Rating" value={formik.values.Rating} onChange={formik.handleChange} min="1" max="5" step="0.1"/></dd>
+            </dl>
+            <button className="btn btn-primary">Add Product</button>
+           </form>
+        </div>
+    )
+}
